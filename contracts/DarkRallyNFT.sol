86d2f8d3b87452f6cb58c2ad8d1df739bd3f69c0
jorger-delfosti <jorge.rojas@delfosti.com>
2023-07-02 18:54:58 -0500

    Added structure

--- contracts/DarkRallyNFT.sol
+++ contracts/DarkRallyNFT.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
 contract DarkRallyNFT is
     Initializable,
@@ -21,17 +22,55 @@ contract DarkRallyNFT is
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
+    bytes32 public constant DARK_TROPHY = keccak256("DARK_TROPHY");
+
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+    CountersUpgradeable.Counter private _tokenIdCounter;
+
+    // Balances per token ID
+    mapping(uint256 => mapping(address => uint256)) private _balances;
+
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    // Token name
+    string internal _name;
+    // Token symbol
+    string internal _symbol;
+    // Token prefix
+    string internal _prefix;
+
+    string private _uri;
+
+    // Struct Nft Info
+    struct NftInfo {
+        bytes32 nftType;
+        bool canTransfer;
+        address scAddress;
+        bool requiredRole;
+        string errorMessage;
+        string errorRegister;
+    }
+    mapping(bytes32 => NftInfo) public _nftInfo;
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() public initializer {
-        __ERC1155_init("");
+    function initialize(
+        string memory name_,
+        string memory symbol_
+    ) public initializer {
+        _prefix = "ipfs://QmbbGgo93M6yr5WSUMj53SWN2vC7L6aMS1GZSPNkVRY8e4/";
+
+        __ERC1155_init(_prefix);
         __AccessControl_init();
         __Pausable_init();
         __ERC1155Burnable_init();
         __UUPSUpgradeable_init();
+
+        _name = name_;
+        _symbol = symbol_;
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(URI_SETTER_ROLE, msg.sender);
